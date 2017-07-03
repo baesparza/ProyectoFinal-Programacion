b1769639727642af7ac34e97d412d0cfb429bfd6
baesparza <bruno.be81@gmail.com>
2017-07-03 10:32:03 -0500

    Insertion Sort

---
+++ src/algoritmos/InsertionSort.java
@@ -0,0 +1,33 @@
+/**
+ * @license
+ * Copyright (c) 2017 Bruno Esparza, Jorge Hurtado
+ * 
+ * Use of this source code is governed by an MIT-style license that can be found in 
+ * the LICENSE file at https://github.com/baesparza/ProyectoFinal-Programacion/blob/master/LICENSE
+ */
+package algoritmos;
+
+public class InsertionSort extends Sort {
+    private String nombre_algoritmo;
+    private String descripcion_algoritmo;
+
+    // constructor por defecto
+    public InsertionSort() {
+    }
+
+    // constructor con los atributos de la clase Sort
+    public InsertionSort(String nombre_algoritmo, String descripcion_algoritmo) {
+        super();
+        this.nombre_algoritmo = nombre_algoritmo;
+        this.descripcion_algoritmo = descripcion_algoritmo;
+    } 
+
+    public void insertionSort() {
+        /**
+         * To sort unordered list of elements, we remove its entries one at a time and then insert 
+         * each of them into a sorted part (initially empty)
+         * Algoritmo ....
+         */
+    }
+
+}
