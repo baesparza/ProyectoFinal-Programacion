6b81ab0ba35947134118d9f84de2ffc0f0dc92fd
baesparza <bruno.be81@gmail.com>
2017-07-03 21:59:20 -0500

    Insertion sort

--- src/algoritmos/InsertionSort.java
+++ src/algoritmos/InsertionSort.java
@@ -1,33 +1,41 @@
 /**
  * @license
  * Copyright (c) 2017 Bruno Esparza, Jorge Hurtado
- * 
- * Use of this source code is governed by an MIT-style license that can be found in 
+ *
+ * Use of this source code is governed by an MIT-style license that can be found in
  * the LICENSE file at https://github.com/baesparza/ProyectoFinal-Programacion/blob/master/LICENSE
  */
 package algoritmos;
 
-public class InsertionSort extends Sort {
-    private String nombre_algoritmo;
-    private String descripcion_algoritmo;
+import Tiempo.Tiempo;
 
-    // constructor por defecto
-    public InsertionSort() {
-    }
+public class InsertionSort extends Sort {
+
+    // timer
+    Tiempo timer = new Tiempo();
 
     // constructor con los atributos de la clase Sort
-    public InsertionSort(String nombre_algoritmo, String descripcion_algoritmo) {
+    public InsertionSort() {
         super();
-        this.nombre_algoritmo = nombre_algoritmo;
-        this.descripcion_algoritmo = descripcion_algoritmo;
-    } 
-
-    public void insertionSort() {
-        /**
-         * To sort unordered list of elements, we remove its entries one at a time and then insert 
-         * each of them into a sorted part (initially empty)
-         * Algoritmo ....
-         */
+        this.nombre_algoritmo = "Insertion Sort";
+        this.descripcion_algoritmo = "Para ordenar una lista de elementos desordenada, eliminamos sus entradas una a una e insertamos Cada uno de ellos en una parte ya ordenada";
     }
 
+    public void sortList() {
+        this.timer.start();
+        for (int i = 0; i < this.lista.length; i++) {
+            int temp = this.lista[i];
+            int j = i - 1;
+            while (j >= 0 && temp < this.lista[j]) {
+                this.lista[j + 1] = this.lista[j];
+                j--;
+                this.cont_procesos++;
+            }
+            this.cont_procesos++;
+
+            this.lista[j + 1] = temp;
+        }
+        this.timer.stop();
+        this.total = this.timer.time();
+    }
 }
