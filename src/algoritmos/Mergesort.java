a8656c36d81ec3fb227ad9f3087f31e476022dee
baesparza <bruno.be81@gmail.com>
2017-07-03 21:59:35 -0500

    Merge Sort

---
+++ src/algoritmos/Mergesort.java
@@ -0,0 +1,33 @@
+
+/**
+ * @license
+ * Copyright (c) 2017 Bruno Esparza, Jorge Hurtado
+ *
+ * Use of this source code is governed by an MIT-style license that can be found in
+ * the LICENSE file at https://github.com/baesparza/ProyectoFinal-Programacion/blob/master/LICENSE
+ */
+package algoritmos;
+
+import Tiempo.Tiempo;
+import java.util.Arrays;
+
+public class Mergesort extends Sort {
+
+    // timer
+    Tiempo timer = new Tiempo();
+
+    // constructor con los atributos de la clase Sort
+    public Mergesort() {
+        super();
+        this.nombre_algoritmo = "Merge Sort";
+        this.descripcion_algoritmo = "Descripcion...";
+    }
+
+    public void sortList() {
+        this.timer.start();
+        Arrays.sort(this.lista);
+        this.timer.stop();
+        this.total = this.timer.time();
+    }
+}
+
