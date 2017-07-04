a66f13f8737a2bff7029fba1c0b865e53d8bd647
baesparza <bruno.be81@gmail.com>
2017-07-03 21:59:53 -0500

    Sort main object

--- src/algoritmos/Sort.java
+++ src/algoritmos/Sort.java
@@ -1,49 +1,63 @@
 /**
  * @license
  * Copyright (c) 2017 Bruno Esparza, Jorge Hurtado
- * 
- * Use of this source code is governed by an MIT-style license that can be found in 
+ *
+ * Use of this source code is governed by an MIT-style license that can be found in
  * the LICENSE file at https://github.com/baesparza/ProyectoFinal-Programacion/blob/master/LICENSE
  */
 package algoritmos;
 
+import java.util.Arrays;
+
 public class Sort {
+   
     // atributos de la clase
     private String nombre_projecto;
     private String descripcion_projecto;
-    private int[] lista = new int[300];
+    public String nombre_algoritmo;
+    public String descripcion_algoritmo;
+    public long total;
+    public int[] lista = new int[30000];
+    public int cont_procesos = 0;
 
     // contructor con parametros
     public Sort() {
         this.nombre_projecto = "Algoritmos de ordenamiento";
-        this.descripcion_projecto = "Uso de diferentes algoritmos para ordenar listas y verificar su velocidad.";
+        this.descripcion_projecto = "Uso de diferentes algoritmos para ordenar listas, verificar su velocidad y numero de procesos.";
     }
 
-    public void cargarLista(opc) {
+    public void cargarLista(int opc) {
         /**
-         * Dependiendo de la opcion que el usuario elija(opc) cargamos 
-         * uno de los 3 tipos de listas para realizar las pruebas.
+         * Dependiendo de la opcion que el usuario elija(opc) cargamos uno de
+         * los 3 tipos de listas para realizar las pruebas.
          */
         switch (opc) {
-            case(1):
+            case (1):
                 // lista semi ordenada
-                for (int i = 0; i < 300; i++) {
-                    this.lista[i] = (int) (Math.random() * n) + 1;
+                for (int i = 0; i < 30000; i++) {
+                    this.lista[i] = (int) (Math.random()*2000);
                 }
                 break;
-            case(2):
+            case (2):
                 // lista desordenada
-                for (int i = 0; i < 300; i++) {
-                    this.lista[i] = (int) (Math.random() * n) + 1;
+                for (int i = 0; i < 30000; i++) {
+                    this.lista[i] = (int) (Math.random()*2000);
                 }
                 break;
-            case(3):
+            case (3):
                 // lista ordenada al reves
-                for (int i = 0; i < 300; i++) {
-                    this.lista[i] = (int) (Math.random() * n) + 1;
+                for (int i = 0; i < 30000; i++) {
+                    this.lista[i] = (int) (Math.random()*2000);
                 }
                 break;
         }
     }
 
+    public void presentar() {
+        System.out.println(this.nombre_algoritmo);
+        System.out.println("Numero de procesos realizados: "+this.cont_procesos);
+        System.out.println(Arrays.toString(this.lista));
+        System.out.println("Promedio: " + this.total/100 + " Milisegundos");
+    }
+    
 }
