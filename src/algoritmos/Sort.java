2621056e481fb6fcaa802379c7f6a3cee00fa4a0
baesparza <bruno.be81@gmail.com>
2017-07-05 20:58:21 -0500

    main Sort obj

--- src/algoritmos/Sort.java
+++ src/algoritmos/Sort.java
@@ -9,29 +9,69 @@ package algoritmos;
 
 import java.util.Arrays;
 
+import generador.generador;
+import java.util.Scanner;
+
 public class Sort {
-   
+
     // atributos de la clase
     private String nombre_projecto;
     private String descripcion_projecto;
-    public String nombre_algoritmo;
-    public String descripcion_algoritmo;
-    public long total;
+    private String nombre_algoritmo;
+    private String descripcion_algoritmo;
+    private long time;
     public int[] lista;
-    public int cont_procesos = 0;
+    private int cont_procesos = 0;
+    private int lista_tamano = 10000;
 
     // contructor con parametros
-    public Sort(int[] lista) {
+    public Sort(String nombre_algoritmo, String descripcion_algoritmo) {
         this.nombre_projecto = "Algoritmos de ordenamiento";
         this.descripcion_projecto = "Uso de diferentes algoritmos para ordenar listas, verificar su velocidad y numero de procesos.";
-        this.lista = lista;
+        this.nombre_algoritmo = nombre_algoritmo;
+        this.descripcion_algoritmo = descripcion_algoritmo;
     }
 
-    public void presentar() {
-        System.out.println(this.nombre_algoritmo);
-        System.out.println("Numero de procesos realizados: "+this.cont_procesos);
-        System.out.println(Arrays.toString(this.lista));
-        System.out.println("Promedio: " + this.total/100 + " Milisegundos");
+    public int[] cargarLista(int opc) {
+        /**
+         * Dependiendo de la opcion que el usuario elija(opc) cargamos uno de
+         * los 3 tipos de listas para realizar las pruebas.
+         */
+
+        Scanner input = new Scanner(System.in);
+
+        // inicializamos el generador
+        generador gen = new generador(this.lista_tamano);
+
+        switch (opc) {
+            case (1):
+                // lista semi ordenada
+                return gen.semiordenada();
+            case (2):
+                // lista desordenada
+                return gen.desordenada();
+            case (3):
+                // lista ordenada al reves
+                return gen.invertida();
+            default:
+                cargarLista(1);
+                break;
+        }
+        return null;
     }
     
+    public void presentar() {
+        System.out.println(this.nombre_algoritmo);
+        System.out.println("Numero de procesos realizados: " + this.cont_procesos);
+        System.out.println(Arrays.toString(this.lista));
+        System.out.println("Promedio: " + this.time / 100 + " Milisegundos...");
+    }
+
+    public void add_cont_procesos() {
+        this.cont_procesos++;
+    }
+
+    public void set_time(long total) {
+        this.time = total;
+    }
 }
