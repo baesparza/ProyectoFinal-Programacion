b888185a6973ef1c473f06ee2546ce8f5a14c2c3
baesparza <bruno.be81@gmail.com>
2017-07-05 20:57:49 -0500

    Java Sort

---
+++ src/algoritmos/JavaSort.java
@@ -0,0 +1,33 @@
+/**
+ * @license
+ * Copyright (c) 2017 Bruno Esparza, Jorge Hurtado
+ *
+ * Use of this source code is governed by an MIT-style license that can be found in
+ * the LICENSE file at https://github.com/baesparza/ProyectoFinal-Programacion/blob/master/LICENSE
+ */
+package algoritmos;
+
+import Tiempo.Tiempo;
+import java.util.Arrays;
+
+public class JavaSort extends Sort {
+
+    // timer
+    Tiempo timer = new Tiempo();
+
+    // constructor con los atributos de la clase Sort
+    public JavaSort(int opc) {
+        super("Java Sort",
+                "Descripcion..."
+        );
+        this.lista = this.cargarLista(opc);
+    }
+
+    public void sortList() {
+        this.timer.start();
+        Arrays.sort(this.lista);
+        this.add_cont_procesos();
+        this.timer.stop();
+        this.set_time(this.timer.time());
+    }
+}
