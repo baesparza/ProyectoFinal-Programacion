1d4cd3225ad7c957013b44fd0035efd9c70df2b8
baesparza <bruno.be81@gmail.com>
2017-07-11 23:22:13 -0500

    Insertion Sort Integer

---
+++ src/algoritmos/InsertionSortInteger.java
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * Copyright (c) 2017 Bruno Esparza, Jorge Hurtado
+ *
+ * Use of this source code is governed by an MIT-style license that can be found in
+ * the LICENSE file at https://github.com/baesparza/ProyectoFinal-Programacion/blob/master/LICENSE
+ */
+package algoritmos;
+
+import Tiempo.Tiempo;
+import java.util.Arrays;
+
+public class InsertionSortInteger extends Sort {
+
+    // timer
+    Tiempo timer = new Tiempo();
+
+    // constructor con los atributos de la clase Sort
+    public InsertionSortInteger(int opc) {
+        super("Insertion Sort Integer ");
+        this.lista = this.cargarLista(opc);
+    }
+
+    public void sortList() {
+        this.timer.start();
+        int[] sortedArray = new int[this.lista.length];
+        sortedArray[0] = this.lista[0];
+
+        for (int i = 1; i < this.lista.length; i++) {
+
+            int lastSortedIndex = i;
+
+            int extractedElement = this.lista[i];
+
+            for (lastSortedIndex = i; lastSortedIndex > 0; lastSortedIndex--) {
+
+                if (sortedArray[lastSortedIndex - 1] > extractedElement) {
+                    sortedArray[lastSortedIndex] = sortedArray[lastSortedIndex - 1];
+                    sortedArray[lastSortedIndex - 1] = extractedElement;
+                    this.add_cont_procesos();
+                } else {
+                    sortedArray[lastSortedIndex] = extractedElement;
+                    this.add_cont_procesos();
+                    break;
+                }
+                this.add_cont_procesos();
+            }
+            this.add_cont_procesos();
+        }
+        this.lista = sortedArray;
+        this.add_cont_procesos();
+        this.timer.stop();
+        this.set_time(this.timer.time());
+    }
+
+    public void run() {
+        this.sortList();
+        this.presentar();
+    }
+}
