c208ce0cd63cba0d53ff788451f3b7690f257ace
baesparza <bruno.be81@gmail.com>
2017-07-11 23:22:53 -0500

    Quick sort

---
+++ src/algoritmos/Quicksort.java
@@ -0,0 +1,66 @@
+/**
+ * @license
+ * Copyright (c) 2017 Bruno Esparza, Jorge Hurtado
+ *
+ * Use of this source code is governed by an MIT-style license that can be found in
+ * the LICENSE file at https://github.com/baesparza/ProyectoFinal-Programacion/blob/master/LICENSE
+ */
+package algoritmos;
+
+import Tiempo.Tiempo;
+
+public class Quicksort extends Sort {
+
+    // timer
+    Tiempo timer = new Tiempo();
+
+    // constructor con los atributos de la clase Sort
+    public Quicksort(int opc) {
+        super("Quick Sort");
+        this.lista = this.cargarLista(opc);
+    }
+
+    public void sortList(int[] array, int inicio, int fin) {
+        this.timer.start();
+
+        int i = inicio, j = fin;
+        if (fin - inicio >= 1) {
+            int pivot = array[fin];
+            while (i < j) {
+                while (array[i] < pivot && i < fin) {
+                    i++;
+                    this.add_cont_procesos();
+                }
+                while (array[j] >= pivot && j > inicio) {
+                    j--;
+                    this.add_cont_procesos();
+                }
+                if (i < j) {
+                    this.swap(array, i, j);
+                    this.add_cont_procesos();
+                }
+            }
+            this.swap(array, fin, i);
+            this.add_cont_procesos();
+            this.sortList(array, inicio, i - 1);
+            this.sortList(array, i + 1, fin);
+            this.add_cont_procesos();
+        } else {
+            return;
+        }
+        this.timer.stop();
+        this.set_time(this.timer.time());
+    }
+
+    public void swap(int[] ar, int index1, int index2) {
+        int temp = ar[index1];
+        ar[index1] = ar[index2];
+        ar[index2] = temp;
+        this.add_cont_procesos();
+    }
+
+    public void run() {
+        this.sortList(this.lista, 0, this.lista.length - 1);
+        this.presentar();
+    }
+}
